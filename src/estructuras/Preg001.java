29c3eed51e96e1e98bd1cd20c75c456857e21fab
Rosa Ayala <1823110336@untels.edu.pe>
2020-06-30 15:27:17 -0500

    login.jsp listo

---
+++ src/estructuras/Preg001.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructuras;
+
+import java.util.Scanner;
+        
+
+/**
+ *
+ * @author Rosa Ayala
+ */
+public class Preg001 {
+    public static void main (String[] args){
+        Scanner teclado  = new Scanner(System.in);
+        int numero;
+        System.out.print("Numero: ");
+        numero = Integer.valueOf(teclado.nextLine());
+        if (numero % 2 == 0){
+            System.out.println("Resultado = par");
+        }
+        else {
+            System.out.println("Resultado = impar");
+        }
+        teclado.close();
+    }
+    
+}
